7ccefa3e14df68e5fdf1de42d143d63dc4cfbfec
Dusan Marilovic <marilo.dusan@gmail.com>
2024-11-19 12:57:46 +0100

    Dodao folder i fajl

---
+++ Projekat/Klassa1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekat
+{
+    public class Klassa1
+    {
+        public sring hello()
+        {
+            string hello = "Hello";
+            return hello;
+        }
+    }
+}
