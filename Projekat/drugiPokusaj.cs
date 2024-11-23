9ab0312efde05d07680766d5010c72a0f4717568
Dusan Marilovic <marilo.dusan@gmail.com>
2024-11-23 15:26:26 +0100

    test2

---
+++ Projekat/drugiPokusaj.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekat
+{
+    public class drugiPokusaj
+    {
+        string goran = "Ja sam gorran i koristim fork";
+        
+    }
+}
